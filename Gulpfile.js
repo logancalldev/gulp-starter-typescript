cab2de7768154ab967f57162c556e738e2701f06
Logan Call <logancall.dev@gmail.com>
2016-12-27 23:26:31 -0700

    added plumber

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,12 +8,16 @@ const uglify = require("gulp-uglify");
 const sourcemaps = require("gulp-sourcemaps");
 const buffer = require("vinyl-buffer");
 const browserSync = require("browser-sync").create();
+const plumber = require("gulp-plumber");
 const typescriptConfig = require("./tsconfig.json"); // config options – https://www.typescriptlang.org/docs/handbook/compiler-options.html
-const dist = "../public/assets"
-const vhost = "http://gulp:80"
+const conf = require("./config.json");
 
 gulp.task("tslint", () =>
 	gulp.src(["scripts/*.ts", "scripts/**/*.ts"])
+		.pipe(plumber(function (error, second) {
+			gutil.log(gutil.colors.red(error.message));
+			this.emit("end");
+		}))
 		.pipe(tslint({
 			formatter: "verbose",
 			configuration: "tslint.json"
@@ -27,25 +31,29 @@ gulp.task("scripts", ["tslint"], () => {
 	return browserify({
 		basedir: ".",
 		debug: true, // allows tsify to do sourcemaping 
-		entries: ["scripts/index.ts"],
+		entries: conf.scriptEntries,
 		cache: {},
 		packageCache: {}
 	})
 		.plugin(tsify, typescriptConfig)
 		.bundle()
+		.pipe(plumber(function (error, second) {
+			gutil.log(gutil.colors.red(error.message));
+			this.emit("end");
+		}))
 		.pipe(source("bundle.js"))
 		.pipe(buffer())
 		.pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(uglify())
 		.pipe(sourcemaps.write("./"))
-		.pipe(gulp.dest(dist))
+		.pipe(gulp.dest(conf.dist))
 		.pipe(browserSync.stream());
 });
 
 gulp.task("watch", ["scripts"], () => {
 	// Serve files from this project"s virtual host that has been configured with the server rendering this site
 	browserSync.init({
-		proxy: vhost,
+		proxy: conf.vhost,
 		files: [
 			{
 				options: {
@@ -53,15 +61,14 @@ gulp.task("watch", ["scripts"], () => {
 				}
 			}
 		],
-		logPrefix: vhost,
+		logPrefix: conf.vhost,
 		reloadOnRestart: true,
 		notify: false
 	});
 
 	gulp.watch(["scripts/**"], ["scripts"]);
 	// gulp.watch( ["styles/**"], ["style"] );
-	// gulp.watch( watch.views ).on("change", browserSync.reload);
-	gulp.watch(["../public/*.html"]).on("change", browserSync.reload);
+	gulp.watch([conf.watchedViews]).on("change", browserSync.reload);
 });
 
 gulp.task("default", ["watch"])
