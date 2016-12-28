de1a5c5030dadf622ffeb07c076132596bfada7a
Logan Call <logancall.dev@gmail.com>
2016-12-28 01:49:40 -0700

    add ignore files to stylelint. other updates...

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,4 +1,8 @@
 /// <reference path="../typings/index.d.ts" />
-import * as $ from 'jquery'
-(<any>window).jQuery = $
-import 'bootstrap-sass'
+import * as $ from "jquery";
+(<any> window).jQuery = $;
+import "bootstrap-sass";
+
+$(document).ready(() => {
+	console.log("index.ts is compiling");
+});
