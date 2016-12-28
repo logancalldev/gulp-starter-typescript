2cdae94b3288923d37295e6327115850cd39fecc
Logan Call <logancall.dev@gmail.com>
2016-12-28 00:55:27 -0700

    added cache busting to css, js, generated iconfont. Fixed iconfont task - added lodash dep

--- Gulpfile.js
+++ Gulpfile.js
@@ -23,6 +23,7 @@ const imagemin = require("gulp-imagemin");
 const conf = require("./config.json");
 const iconfont = require("gulp-iconfont");
 const consolidate = require("gulp-consolidate");
+const bust = require('gulp-cache-bust');
 
 gulp.task("tslint", () =>
 	gulp.src(["scripts/*.ts", "scripts/**/*.ts"])
@@ -57,6 +58,7 @@ gulp.task("scripts", ["tslint"], () => {
 		.pipe(buffer())
 		.pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(uglify())
+		.pipe(bust())
 		.pipe(sourcemaps.write("./"))
 		.pipe(gulp.dest(conf.dist))
 		.pipe(browserSync.stream());
@@ -76,7 +78,7 @@ gulp.task("sass-lint", () => {
 			gutil.log(gutil.colors.red(error.message));
 			this.emit("end");
 		}))
-		.pipe(postcss(processors, {syntax: syntax_scss}));
+		.pipe(postcss(processors, { syntax: syntax_scss }));
 })
 
 gulp.task("styles", ["sass-lint"], () => {
@@ -90,6 +92,7 @@ gulp.task("styles", ["sass-lint"], () => {
 		.pipe(rucksack({ autoprefixer: true }))
 		.pipe(cssnano())
 		.pipe(rename("bundle.css"))
+		.pipe(bust())
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest(conf.dist))
 		.pipe(browserSync.stream());
@@ -121,33 +124,42 @@ gulp.task("images", () => {
 		.pipe(imagemin({
 			progressive: true,
 			interlaced: true,
-			svgoPlugins: [{removeUnknownsAndDefaults: false}, {cleanupIDs: false}]
+			svgoPlugins: [{ removeUnknownsAndDefaults: false }, { cleanupIDs: false }]
 		}))
 		.pipe(gulp.dest(conf.dist))
 })
 
 // reference for svg setup: https://github.com/nfroidure/gulp-iconfont
 gulp.task("iconfonts", () => {
-  return gulp.src("fonts/icons/svg/**")
-    .pipe(iconfont({ fontName: "website-icons" }))
-    .on("glyphs", function(glyphs, options) {
-      gulp.src("fonts/icons/iconfont.template")
-        .pipe(consolidate("lodash", {
-          glyphs: glyphs,
-          fontName: "website-icons",
-          fontPath: "../fonts/",
-          timestamp: Date.now(),
-          className: "icon"
-        }))
-        .pipe(rename("iconfont.scss"))
-        .pipe(gulp.dest("styles/"));
-    })
-    .pipe(gulp.dest(conf.dist + "fonts"));
+	return gulp.src("fonts/icons/svg/**")
+		.pipe(plumber(function (error) {
+			gutil.log(gutil.colors.red(error.message));
+			this.emit("end");
+		}))
+		.pipe(iconfont({ fontName: "website-icons" }))
+		.on("glyphs", function (glyphs, options) {
+			gulp.src("fonts/icons/iconfont.template")
+				.pipe(plumber(function (error) {
+					gutil.log(gutil.colors.red(error.message));
+					this.emit("end");
+				}))
+				.pipe(consolidate("lodash", {
+					glyphs: glyphs,
+					fontName: "website-icons",
+					fontPath: "../fonts/",
+					timestamp: Date.now(),
+					className: "icon"
+				}))
+				.pipe(rename("iconfont.scss"))
+				.pipe(gulp.dest("styles/"));
+		})
+		.pipe(bust())
+		.pipe(gulp.dest(conf.dist + "fonts"));
 });
 
 gulp.task("fonts", ["iconfonts"], () => { // in case we have other font libraries that need to be but in assets... put them here and then they'll get copied over
-  return gulp.src("fonts/vendor/**")
-    .pipe(gulp.dest( conf.dist + "fonts" ))
+	return gulp.src("fonts/vendor/**")
+		.pipe(gulp.dest(conf.dist + "fonts"))
 });
 
 gulp.task("default", ["images", "fonts", "watch"])
