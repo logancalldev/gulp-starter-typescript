851510599d3c11a365afd720969ce45ba6be94a9
Logan Call <logancall.dev@gmail.com>
2016-12-27 21:38:37 -0700

    add tsconfig to gulpfile scripts task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,45 +1,44 @@
 const gulp = require("gulp");
 const browserify = require("browserify");
-const source = require('vinyl-source-stream');
+const source = require("vinyl-source-stream");
 const tsify = require("tsify");
 const gutil = require("gulp-util");
-const uglify = require('gulp-uglify');
-const sourcemaps = require('gulp-sourcemaps');
-const buffer = require('vinyl-buffer');
-const browserSync	= require('browser-sync').create();
+const uglify = require("gulp-uglify");
+const sourcemaps = require("gulp-sourcemaps");
+const buffer = require("vinyl-buffer");
+const browserSync	= require("browser-sync").create();
+const typescriptConfig = require("./tsconfig.json");
 const dist = "../public/assets"
 const vhost = "http://gulp:80"
-// const ts = require("gulp-typescript");
-// const tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("scripts", function () {
+gulp.task("scripts", () => {
 	return browserify({
-		basedir: '.',
+		basedir: ".",
 		debug: true, // allows tsify to do sourcemaping 
-		entries: ['scripts/index.ts'],
+		entries: ["scripts/index.ts"],
 		cache: {},
 		packageCache: {}
 	})
-		.plugin(tsify)
+		.plugin(tsify, typescriptConfig)
 		.bundle()
-		.pipe(source('bundle.js'))
+		.pipe(source("bundle.js"))
 		.pipe(buffer())
 		.pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(uglify())
-		.pipe(sourcemaps.write('./'))
+		.pipe(sourcemaps.write("./"))
 		.pipe(gulp.dest(dist))
 		.pipe(browserSync.stream());
 });
 
-gulp.task('default', ['scripts'], function() {
+gulp.task("watch", ["scripts"], () => {
 
-	// Serve files from this project's virtual host that has been configured with the server rendering this site
+	// Serve files from this project"s virtual host that has been configured with the server rendering this site
 	browserSync.init({
 		proxy : vhost,
 		files: [
 			{
 				options: {
-					ignored: '.*'
+					ignored: ".*"
 				}
 			}
 		],
@@ -48,8 +47,10 @@ gulp.task('default', ['scripts'], function() {
 		notify: false
 	});
 
-	gulp.watch( ['scripts/**'], ["scripts"] );
+	gulp.watch( ["scripts/**"], ["scripts"] );
 	// gulp.watch( watch.styles, ["style"] );
 	// gulp.watch( watch.views ).on("change", browserSync.reload);
-	gulp.watch( ['../public/*.html'] ).on("change", browserSync.reload);
+	gulp.watch( ["../public/*.html"] ).on("change", browserSync.reload);
 });
+
+gulp.task("default", ["watch"])
